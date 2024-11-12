cde63fa5d7c5e8994afc565801e0d3d0b76a0518
steklon <steklo_one@mail.ru>
2024-11-13 00:16:04 +1000

    fixed file eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -34,6 +34,7 @@ export default [
             'no-unused-vars': 'off', // не испоьзуемые переменные
             'no-console': 'off', // console.log
             'no-var': 'error',
+            'constructor-super': 'off',
         },
     },
     {
